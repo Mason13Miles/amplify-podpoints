ac04495fae48aa132ff4f3e088fdcaa2a57705ea
Mason Miles <masonbmiles@icloud.com>
2025-01-27 10:59:51 -0700

    fixed compilation errors

--- app/components/ui/checkbox.tsx
+++ app/components/ui/checkbox.tsx
@@ -4,12 +4,12 @@ import * as React from 'react';
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
 import { Check } from 'lucide-react';
 
-import { cn } from '@/lib/utils';
+import { cn } from '../../lib/utils';
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({ className, ...props }, ref) => (
+>(({ className = '', ...props }, ref) => (
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
